033378b6bd5df0d747c760b0ab76e2fbf8dba801
Rakesh Shrestha <rakeshsh31@gmail.com>
2024-03-24 20:00:48 -0700

    move bounding box computation to preprocess_koolai_data

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -16,12 +16,11 @@ import trimesh
 from transforms3d import utils, axangles
 
 from geometry_utils import create_spatial_quad_polygen
-from scripts.preprocess_koolai_data import parse_user_output, adjust_cam_meta, parse_room_meta
-
-
-SCALE = 0.001
-SCALE_MAT = np.eye(4)
-SCALE_MAT[range(3), range(3)] = SCALE
+from scripts.preprocess_koolai_data import (
+    parse_user_output, adjust_cam_meta, parse_room_meta,
+    get_room_lineset, get_room_bounds, obbs_to_pix,
+    SCALE, SCALE_MAT, R_wsu_cv, T_wsu_cv,
+)
 
 
 def getCameraT(camera_meta_dict):
@@ -53,144 +52,7 @@ def getCameraT(camera_meta_dict):
     return T
 
 
-def get_room_lineset(room_meta_dict: Dict[str, List[Dict[str, float]]]) -> o3d.geometry.LineSet:
-    lineset = o3d.geometry.LineSet()
-    points = []
-    for edge in room_meta_dict['edge']:
-        start = edge['start']
-        start = [start['x'], start['y'], start['z']]
-        end = edge['end']
-        end = [end['x'], end['y'], end['z']]
-        points.append(start)
-        points.append(end)
-
-    points = np.array(points)
-    lines = np.asarray([[2 * i, 2 * i + 1] for i in range(points.shape[0] // 2)])
-    lineset.points = o3d.utility.Vector3dVector(points)
-    lineset.lines = o3d.utility.Vector2iVector(lines)
-
-    return lineset
-
-
-def get_room_bounds(room_meta):
-    room_corners = []
-    inds = []
-    for point in room_meta['edge']:
-        room_corners.append([point['start']['x'], point['start']['y'], point['start']['z']])
-        room_corners.append([point['end']['x'], point['end']['y'], point['end']['z']])
-    room_corners = np.array(room_corners)
-    bbox_min = np.min(room_corners, axis=0)
-    bbox_max = np.max(room_corners, axis=0)
-    return bbox_min, bbox_max
-
-
-def interpolate_line(p1, p2, num=30):
-    t = np.expand_dims(np.linspace(0, 1, num=num, dtype=np.float32), 1)
-    points = p1 * (1 - t) + t * p2
-    return points
-
-
-def cam3d2rad(cam3d):
-    """
-    Transform 3D points in camera coordinate to longitude and latitude.
-
-    Parameters
-    ----------
-    cam3d: n x 3 numpy array or bdb3d dict
-
-    Returns
-    -------
-    n x 2 numpy array of longitude and latitude in radiation
-    first rotate left-right, then rotate up-down
-    longitude: (left) -pi -- 0 --> +pi (right)
-    latitude: (up) -pi/2 -- 0 --> +pi/2 (down)
-    """
-    backend, atan2 = (torch, torch.atan2) if isinstance(cam3d, torch.Tensor) else (np, np.arctan2)
-    lon = atan2(cam3d[..., 0], cam3d[..., 2])
-    lat = backend.arcsin(cam3d[..., 1] / backend.linalg.norm(cam3d, axis=-1))
-    return backend.stack([lon, lat], -1)
-
-
-def camrad2pix(camrad, width, height):
-    """
-    Transform longitude and latitude of a point to panorama pixel coordinate.
-
-    Parameters
-    ----------
-    camrad: n x 2 numpy array
-
-    Returns
-    -------
-    n x 2 numpy array of xy coordinate in pixel
-    x: (left) 0 --> (width - 1) (right)
-    y: (up) 0 --> (height - 1) (down)
-    """
-    if isinstance(camrad, torch.Tensor):
-        campix = torch.empty_like(camrad, dtype=torch.float32)
-    else:
-        campix = np.empty_like(camrad, dtype=np.float32)
-    campix[..., 0] = camrad[..., 0] * width / (2. * np.pi) + width / 2. + 0.5
-    campix[..., 1] = camrad[..., 1] * height / np.pi + height / 2. + 0.5
-    return campix
-
-
-def cam3d2pix(cam3d, width, height):
-    """
-    Transform 3D points from camera coordinate to pixel coordinate.
-
-    Parameters
-    ----------
-    cam3d: n x 3 numpy array or bdb3d dict
-
-    Returns
-    -------
-    for 3D points: n x 2 numpy array of xy in pixel.
-    x: (left) 0 --> width - 1 (right)
-    y: (up) 0 --> height - 1 (down)
-    """
-    campix = camrad2pix(cam3d2rad(cam3d), width, height)
-    return campix
-
-
-def obbs_to_pix(bboxes, width, height, fov=None, num=300):
-    cam3d = []
-    for bbox in bboxes:
-        T = np.array(bbox["transform"]).reshape(4, 4)
-        size = np.array(bbox["size"])
-        obb = o3d.geometry.OrientedBoundingBox(T[:3, 3:4], T[:3, :3], size)
-        points = obb.get_box_points()
-        box_lines = [
-            [0, 1], [1, 7], [7, 2], [2, 0],
-            [3, 6], [6, 4], [4, 5], [5, 3],
-            [0, 3], [2, 5], [1, 6], [7, 4]
-        ]
-        for line in box_lines:
-            p1 = points[line[0]]
-            p2 = points[line[1]]
-            cam3d.extend(interpolate_line(p1, p2, num=num).tolist())
-
-    cam3d = np.array(cam3d)
-    if fov is not None:
-        fov = np.radians(fov)
-        focal = width / (2 * np.tan(fov / 2))
-        K = np.array([
-            [focal, 0, width / 2],
-            [0, focal, height / 2],
-            [0, 0, 1]
-        ])
-        pix = cam3d @ K.T
-        pix = pix[:, :2] / pix[:, 2:]
-    else:
-        pix = cam3d2pix(cam3d, width, height)
-    # clip
-    pix = np.clip(pix, 0, [width - 1, height - 1])
-    pix = pix.astype(int)
-
-    return pix
-
-
 scene_folderpath = Path("/mnt/Exp/rakesh/LRM_pano/20240229_data/3FO4K5GA97T0/")
-ins_meta_file = scene_folderpath / "ins_meta.json"
 structure_json_path = next(scene_folderpath.glob("*_structure/user_output.json"))
 render_paths = scene_folderpath.glob("*_render/*")
 render_path = next(render_paths).parents[0]
@@ -198,22 +60,18 @@ output_dir = os.path.join(str(scene_folderpath), 'panorama')
 # cam_idx = 15
 # camera_id_str = f"629_{cam_idx}"
 
-with open(ins_meta_file, "r") as f:
-    ins_meta = json.load(f)
+meta_data_dict = parse_user_output(structure_json_path)
+ins_meta = meta_data_dict['instance_meta']
 
-mesh = o3d.geometry.TriangleMesh()
 obbs = []
 for bbox in ins_meta:
     T = np.array(bbox["transform"]).reshape(4, 4)
     size = np.array(bbox["size"])
     obb = trimesh.creation.box(extents=size, transform=T)
     obbs.append(obb)
-
 obbs = trimesh.util.concatenate(obbs)
 obbs.apply_transform(SCALE_MAT)
 obbs.export(f'/tmp/bbox_all.ply')
-
-meta_data_dict = parse_user_output(structure_json_path)
 
 room_meta_dict = {}
 for room_meta in meta_data_dict['room_meta']:
@@ -232,16 +90,6 @@ R_cv_kool = np.array([
 ]).reshape((3, 3))
 R_kool_cv = R_cv_kool.T
 
-# WSU: West (left), South (back), Up coordinate system
-R_cv_wsu = np.array([
-    -1, 0, 0,
-    0, 0, -1,
-    0, -1, 0
-]).reshape((3, 3))
-R_wsu_cv = R_cv_wsu.T
-T_wsu_cv = np.eye(4)
-T_wsu_cv[:3, :3] = R_wsu_cv
-
 camera_stat_dict = defaultdict(list)
 
 for camera_meta_dict in meta_data_dict['camera_meta']:
@@ -259,6 +107,7 @@ for camera_meta_dict in meta_data_dict['camera_meta']:
 
     # new camera meta data
     new_cam_meta_idct = adjust_cam_meta(raw_cam_meta_dict=camera_meta_dict,
+                                        instance_meta=meta_data_dict['instance_meta'],
                                         room_id=int(room_id_str),
                                         new_cam_id=new_cam_id_in_room,
                                         new_img_height=target_pano_height,
@@ -268,7 +117,7 @@ for camera_meta_dict in meta_data_dict['camera_meta']:
     T_cw = np.array(new_cam_meta_idct["camera_transform"]).reshape((4, 4))
     T_wc = np.linalg.inv(T_cw)
     T_wc[:3, 3] = T_wc[:3, 3]
-    T_wc[:3, :3] = T_wc[:3, :3] @ R_wsu_cv
+    T_wc[:3, :3] = T_wc[:3, :3]
     T_cw = np.linalg.inv(T_wc)
 
     # T_wc = getCameraT(camera_meta_dict)
@@ -303,11 +152,11 @@ for camera_meta_dict in meta_data_dict['camera_meta']:
     room_obbs = trimesh.util.concatenate(room_obbs)
     room_obbs.export(f'/tmp/bbox_room_{camera_id_str}.ply')
 
-    room_obbs.apply_transform(T_wsu_cv @ T_cw)
+    room_obbs.apply_transform(T_cw)
     room_obbs.export(f'/tmp/bbox_room_cam_{camera_id_str}.ply')
 
     lineset = deepcopy(room_meta['lineset'])
-    lineset = lineset.transform(T_wsu_cv @ T_cw)
+    lineset = lineset.transform(T_cw)
     o3d.io.write_line_set(f'/tmp/walls_cam_{camera_id_str}.ply', lineset)
 
     # img_path = osp.join(render_path, camera_id_str, "cubic.jpg")
